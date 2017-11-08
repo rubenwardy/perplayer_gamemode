2b9289a2669fe4a61c2255a506690439e18209d7
rdococ <ryandoc08@hotmail.com>
2017-11-08 20:47:20 +0000

    Added deadly and management modes

--- init.lua
+++ init.lua
@@ -1,17 +1,34 @@
-perplayer_gamemode = { users = {} }
+perplayer_gamemode = { creative = {}, damage = {} }
 
 dofile(minetest.get_modpath("perplayer_gamemode") .. "/ChatCmdBuilder.lua")
 
-function perplayer_gamemode.is_enabled_for(name)
-	if perplayer_gamemode.users[name] == nil then
+function perplayer_gamemode.is_in_creative_mode(name)
+	if perplayer_gamemode.creative[name] == nil then
 		return minetest.setting_getbool("creative_mode")
 	else
-		return perplayer_gamemode.users[name]
+		return perplayer_gamemode.creative[name]
+	end
+end
+function perplayer_gamemode.can_take_damage(name)
+	if perplayer_gamemode.creative[name] == nil then
+		return minetest.setting_getbool("enable_damage")
+	else
+		return perplayer_gamemode.damage[name]
 	end
 end
 
 function perplayer_gamemode.set_creative(name, v)
-	perplayer_gamemode.users[name] = v
+	perplayer_gamemode.creative[name] = v
+
+	local player = minetest.get_player_by_name(name)
+	if player then
+		local context = sfinv.contexts[name]
+		context.page = sfinv.get_homepage_name(player)
+		sfinv.set_player_inventory_formspec(player)
+	end
+end
+function perplayer_gamemode.set_damage(name, v)
+	perplayer_gamemode.damage[name] = v
 
 	local player = minetest.get_player_by_name(name)
 	if player then
@@ -22,19 +39,19 @@ function perplayer_gamemode.set_creative(name, v)
 end
 
 if creative.is_enabled_for then
-	creative.is_enabled_for = perplayer_gamemode.is_enabled_for
+	creative.is_enabled_for = perplayer_gamemode.is_in_creative_mode
 else
 	for name, def in pairs(sfinv.pages) do
 		if #name > 9 and name:sub(1, 9) == "creative:" then
 			def.is_in_nav = function(self, player, context)
-				return perplayer_gamemode.is_enabled_for(player:get_player_name())
+				return perplayer_gamemode.is_in_creative_mode(player:get_player_name())
 			end
 		end
 	end
 
 	local old_homepage_name = sfinv.get_homepage_name
 	function sfinv.get_homepage_name(player)
-		if perplayer_gamemode.is_enabled_for(player:get_player_name()) then
+		if perplayer_gamemode.is_in_creative_mode(player:get_player_name()) then
 			return "creative:all"
 		else
 			return "sfinv:crafting"
@@ -42,23 +59,39 @@ else
 	end
 end
 
-minetest.register_privilege("gamemode", "Can set own creative mode")
-minetest.register_privilege("gamemode_super", "Can set anyone's creative mode")
+minetest.register_privilege("gamemode", "Can set own gamemode")
+minetest.register_privilege("gamemode_super", "Can set anyone's gamemode")
+
+local GamemodeNames = {}
+
+-- Gamemodes with creative mode (infinite blocks)
+GamemodeNames[true] = {}
+
+GamemodeNames[true][false] = "creative"  -- Free resources. Damage is disabled.
+GamemodeNames[true][true] = "deadly"  -- Free resources. Damage is enabled.
+
+-- Gamemodes without creative mode
+GamemodeNames[false] = {}
+
+GamemodeNames[false][false] = "management"  -- No free resources. Damage is disabled.
+GamemodeNames[false][true] = "survival"  -- No free resources. Damage is enabled.
 
 local function is_creative(str)
-	return minetest.is_yes(str) or str == "creative"
+	return str:sub(1, 1) == "c" or str:sub(1, 1) == "d" or str:sub(1, 1) == "0"
+end
+
+local function is_damage(str)
+	return str:sub(1, 1) == "d" or str:sub(1, 1) == "s" or str:sub(1, 1) == "0"
 end
 
 perplayer_gamemode.ChatCmdBuilder.new("gamemode", function(cmd)
 	cmd:sub(":value", function(name, value)
 		if minetest.check_player_privs(name, { gamemode = true }) then
-			local v = is_creative(value)
-			perplayer_gamemode.set_creative(name, v)
-			if v then
-				return true, "Turned creative mode on"
-			else
-				return true, "Turned creative mode off"
-			end
+			local isCreative, isDamage = is_creative(value), is_damage(value)
+			perplayer_gamemode.set_creative(name, isCreative)
+			perplayer_gamemode.set_damage(name, isDamage)
+			return true, "Set gamemode to " .. GamemodeNames[isCreative][isDamage]
+				.. ". Creative = " .. tostring(isCreative) .. ", damage = " .. tostring(isDamage)
 		else
 			return false, "Missing privs: gamemode"
 		end
@@ -69,14 +102,21 @@ perplayer_gamemode.ChatCmdBuilder.new("gamemode", function(cmd)
 			local v = is_creative(value)
 			perplayer_gamemode.set_creative(username, v)
 			if v then
-				return true, "Turned creative mode on for " .. username
+				return true, "Set " .. username .. "'s gamemode to creative"
 			else
-				return true, "Turned creative mode off for " .. username
+				return true, "Set " .. username .. "'s gamemode to survival"
 			end
 		else
 			return false, "Missing privs: gamemode_super"
 		end
 	end)
 end, {
-	description = "Set game mode (creative or survival)"
+	description = "Set gamemode (creative or survival)"
 })
+
+minetest.register_on_player_hpchange(function (player, hp_change)
+	if not perplayer_gamemode.can_take_damage(player:get_player_name()) then
+		return 0, true
+	end
+	return hp_change
+end, true)
